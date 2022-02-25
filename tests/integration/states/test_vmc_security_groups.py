eb3a87db81ce20122cb9e5028782266f4d53a9bd
Nava Patel <pnaval@vmware.com>
2022-02-25 16:37:15 +0530

    Corrected it tests

--- tests/integration/states/test_vmc_security_groups.py
+++ tests/integration/states/test_vmc_security_groups.py
@@ -7,8 +7,6 @@ import pytest
 import requests
 from saltext.vmware.utils import vmc_request
 
-from tests.integration.conftest import get_config
-
 
 @pytest.fixture()
 def security_groups_test_data():
@@ -20,47 +18,88 @@ def security_groups_test_data():
 
 
 @pytest.fixture
-def delete_security_group(vmc_nsx_connect, security_groups_test_data):
+def request_headers(common_data):
+    return vmc_request.get_headers(common_data["refresh_key"], common_data["authorization_host"])
+
+
+@pytest.fixture
+def security_group_url(common_data):
+    url = (
+        "https://{hostname}/vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
+        "policy/api/v1/infra/domains/{domain_id}/groups/{security_group_id}"
+    )
+    api_url = url.format(
+        hostname=common_data["hostname"],
+        org_id=common_data["org_id"],
+        sddc_id=common_data["sddc_id"],
+        domain_id=common_data["domain_id"],
+        security_group_id=common_data["security_group_id"],
+    )
+    return api_url
+
+
+@pytest.fixture
+def security_groups_list_url(common_data):
+    url = (
+        "https://{hostname}/vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
+        "policy/api/v1/infra/domains/{domain_id}/groups"
+    )
+    api_url = url.format(
+        hostname=common_data["hostname"],
+        org_id=common_data["org_id"],
+        sddc_id=common_data["sddc_id"],
+        domain_id=common_data["domain_id"],
+    )
+    return api_url
+
+
+@pytest.fixture
+def common_data(vmc_nsx_connect):
+    hostname, refresh_key, authorization_host, org_id, sddc_id, verify_ssl, cert = vmc_nsx_connect
+    data = {
+        "hostname": hostname,
+        "refresh_key": refresh_key,
+        "authorization_host": authorization_host,
+        "org_id": org_id,
+        "sddc_id": sddc_id,
+        "domain_id": "cgw",
+        "security_group_id": "Integration_SG_1",
+        "verify_ssl": verify_ssl,
+        "cert": cert,
+    }
+    yield data
+
+
+@pytest.fixture
+def get_security_groups(common_data, security_groups_list_url, request_headers):
+    session = requests.Session()
+    response = session.get(
+        url=security_groups_list_url,
+        verify=common_data["cert"] if common_data["verify_ssl"] else False,
+        headers=request_headers,
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+@pytest.fixture
+def delete_security_group(get_security_groups, security_group_url, request_headers, common_data):
     """
     Sets up test requirements:
     Queries vmc api for security groups
     Deletes security group if exists
     """
-    hostname, refresh_key, authorization_host, org_id, sddc_id, verify_ssl, cert = vmc_nsx_connect
-    domain_id, security_group_id, updated_display_name, updated_tags = security_groups_test_data
 
-    url = (
-        "https://{hostname}/vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
-        "policy/api/v1/infra/domains/{domain_id}/groups"
-    )
-    api_url = url.format(hostname=hostname, org_id=org_id, sddc_id=sddc_id, domain_id=domain_id)
-    session = requests.Session()
-    headers = vmc_request.get_headers(refresh_key, authorization_host)
-
-    response = session.get(url=api_url, verify=cert if verify_ssl else False, headers=headers)
-    response.raise_for_status()
-    security_rules_dict = response.json()
-    if security_rules_dict["result_count"] != 0:
-        results = security_rules_dict["results"]
-        for result in results:
-            if result["id"] == security_group_id:
-                url = (
-                    "https://{hostname}/vmc/reverse-proxy/api/orgs/{org_id}/sddcs/{sddc_id}/"
-                    "policy/api/v1/infra/domains/{domain_id}/groups/{security_group_id}"
-                )
-
-                api_url = url.format(
-                    hostname=hostname,
-                    org_id=org_id,
-                    sddc_id=sddc_id,
-                    domain_id=domain_id,
-                    security_group_id=security_group_id,
-                )
-                response = session.delete(
-                    url=api_url, verify=cert if verify_ssl else False, headers=headers
-                )
-                # raise error if any
-                response.raise_for_status()
+    for result in get_security_groups.get("results", []):
+        if result["id"] == common_data["security_group_id"]:
+            session = requests.Session()
+            response = session.delete(
+                url=security_group_url,
+                verify=common_data["cert"] if common_data["verify_ssl"] else False,
+                headers=request_headers,
+            )
+            # raise error if any
+            response.raise_for_status()
 
 
 def test_vmc_security_groups_state_module(
